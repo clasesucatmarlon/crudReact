34657e28372c2e7ba778566ad775a793efac17a1
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-01-14 14:22:23 -0500

    file of test javascript

---
+++ test.js
@@ -0,0 +1,23 @@
+/* sum(10,20)
+diff(10, 20)
+function sum(x, y) {
+    return x + y
+}
+let diff = function(x,y) {
+    return x - y
+}
+
+
+document.body.innerHTML = `<h2>marlon: ${x}</h2>` */
+
+/* for (var i = 0; i < 5; i++) {
+    console.log(i)
+} */
+
+/* var a; var b= (a = 3) ? true : false */
+
+/* console.log(typeof(42)) */
+console.log([3] == [3])
+console.log(3==='3')
+console.log(3=='3')
+console.log(3 != '3')
